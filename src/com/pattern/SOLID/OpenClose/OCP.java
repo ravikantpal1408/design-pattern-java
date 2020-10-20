6f3ec9fa3e051fecafce74ed196f9d433d5af36d
Ravi Pal <ravipal@Ravis-MacBook-Air.local>
2020-10-20 14:41:46 +0530

    violation of open-close pricipal

--- src/com/pattern/SOLID/OpenClose/OCP.java
+++ src/com/pattern/SOLID/OpenClose/OCP.java
@@ -44,5 +44,19 @@ class ProductFilter {
     }
 }
 
+
+// here in this scenario we are violating the principal of open-close .
+// Now we will fix this using specification pattern
 public class OCP {
+    public static void main(String[] args) {
+        Product apple = new Product("Apple", Color.GREEN, Size.SMALL);
+        Product tree = new Product("Tree", Color.GREEN, Size.LARGE);
+        Product house = new Product("House", Color.BLUE, Size.LARGE);
+
+        List<Product> products = List.of(apple, tree, house);
+
+        ProductFilter pf = new ProductFilter();
+        System.out.println("Green products (old)");
+        pf.filterByColor(products, Color.GREEN).forEach(p-> System.out.println(" - " + p.name + " is green "));
+    }
 }
