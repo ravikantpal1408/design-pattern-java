0c4ea7a31297794f545b3b6b0e19bfe1cf3a4780
ravikantpal1408 <ravikantpal123@gmail.com>
2020-10-26 22:55:22 +0530

    started the builder pattern

--- src/com/pattern/builder/BuilderPattern.java
+++ src/com/pattern/builder/BuilderPattern.java
@@ -1,5 +1,27 @@
 package com.pattern.builder;
 
-public class BuilderPattern {
+class HtmlElement {
+    public String name, text;
+}
 
+public class BuilderPattern {
+    public static void main(String[] args) {
+        String hello = "hello"; // this is immutable object
+        System.out.println("<p>" + hello + "</p>");
+
+        String[] words = {"Hello", "World"};
+
+//        System.out.println(
+//                "<ul>\n" + "<li>" + words[0]
+//        );
+        // String builder is used to build a string
+
+        StringBuilder sb = new StringBuilder(); // Piece wise construction
+        sb.append("<ul>\n");
+        for (String word: words) {
+            sb.append(String.format("  <li>%s</li>\n", word));
+        }
+        sb.append("</ul>");
+        System.out.println(sb);
+    }
 }
